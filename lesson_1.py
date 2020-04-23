979f1a597b96b70af70bdf89784d8570605a0af2
roomba69 <rodichev185@yandex.ru>
2020-04-23 22:00:27 +0300

    lesson_1

---
+++ lesson_1.py
@@ -0,0 +1,87 @@
+# print('Hello World!') # комментарий
+
+# print(1+1)
+# print(2*3)
+
+# print(2**3)
+# print(10//3)
+# print(14%3)
+
+# a = 10
+# b = 5
+# c = a= b
+# print (c)
+
+# current_time = 5
+
+# строки str
+# my_str = 'bla'
+# print(type(my_str))
+#
+# целое число
+# my_int = 5
+# print(type(my_int))
+
+# дробное число
+# my_float = 10.4
+# print(type(my_float))
+
+# Булевые значения
+# my_bool = False
+# print(type(my_bool))
+
+# bla = 10
+# bla = 'qwe'
+# print(bla)
+
+# print(1 > 10)
+# print(1 < 10)
+# print(1==10)
+# print(10==10)
+# print(1 != 10)
+# print(10 != 10)
+# print(1<10 and 1==1)
+# print(not(1<10 or 1==1))
+
+# answer = int(input('Введите число: '))
+# print(answer + 10)
+
+# org_pass = '123'
+# admin_pass = 'admin'
+# user_pass = input('Password: ')
+# if user_pass == org_pass:
+#     print('Accept')
+#     admin_check = input('Password admin: ')
+#     if admin_check == admin_pass:
+#         print('Full accept')
+#     else:
+#         print('Just accept')
+# else:
+#     print('Allert')
+
+# color = 'red'
+#
+# if color == 'red':
+#     print('Красный')
+# elif color == 'blue':
+#     print('Синий')
+# elif color == 'white':
+#     print('Белый')
+# else:
+#     print('Неизвестный цвет')
+
+# i = 0
+# while True:
+#     i = i + 1
+#     if i == 5:
+#         continue
+#     print(i)
+#     if i == 20:
+#         break
+
+name = 'Dmitriy'
+surname = 'Burdasov'
+age = 69
+
+print(name, surname, age)
+print('Имя: {} Фамилия: {} Возраст {}'.format(name,surname,age))
